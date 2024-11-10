1607dc5902536f9a99f5ee7c0ef6242045205551
its2ru3 <119913413+its2ru3@users.noreply.github.com>
2024-11-10 23:32:20 +0530

    modified:   utils/dct.py 	modified:   utils/dwt.py 	renamed:    utils/filteringAttacks.py -> utils/filtering-attacks.py 	renamed:    utils/geometricalAttacks.py -> utils/geometrical-attacks.py 	renamed:    utils/imageEnhancementAttacks.py -> utils/image-enhancement-attacks.py 	renamed:    utils/jpegCompression.py -> utils/jpeg-compression.py 	renamed:    utils/noiseAttacks.py -> utils/noise-attacks.py 	renamed:    utils/zig_zag.py -> utils/zig-zag.py

--- utils/dct.py
+++ utils/dct.py
@@ -1,32 +1,19 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
-
-def scale(image):
-    current_max = np.max(image)
-    current_min = np.min(image)
-    scaled_image = ((image-current_min) / (current_max-current_min)) * 255
-    return scaled_image
-
-
+""""""
 # dct function
-def dct(array):
-    n = array.shape[0]
+def dct(img:np.ndarray) -> np.ndarray:
+    n = img.shape[0]
     l = np.arange(n)[:, np.newaxis]
     r = np.arange(n)
     
     cos_matrix = np.cos(np.pi * (2 * r + 1) * l / (2 * n))
-    
     cos_matrix[0, :] *= 1 / np.sqrt(2)
-    
     cos_matrix = np.sqrt(2 / n) * cos_matrix
-    
-    return np.dot(cos_matrix, array)
-
+    return np.dot(cos_matrix, img)
 
 # inverse dct function
-def idct(array):
-    n = array.shape[0]
+def idct(img:np.ndarray) -> np.ndarray:
+    n = img.shape[0]
     l = np.arange(n)[:, np.newaxis]
     r = np.arange(n)
     
@@ -35,5 +22,5 @@ def idct(array):
     cos_matrix[:, 0] *= 1 / np.sqrt(2)
     cos_matrix = np.sqrt(2 / n) * cos_matrix
     
-    return np.dot(cos_matrix, array)
+    return np.dot(cos_matrix, img)
 
