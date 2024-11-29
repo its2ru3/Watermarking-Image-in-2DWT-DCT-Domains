4d7f80080671ff78227de82dc60e096673321067
projectsvy67 <vishal1804y@gmail.com>
2024-11-29 21:46:56 +0530

    Made some changes in attacks part

---
+++ wm2dwt/utils/noise_attacks.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+import numpy as np
+
+# Gaussian Noise
+def gaussian_noise_adder(img, std_dev, mean=0):
+    noise = np.random.normal(mean, std_dev, img.shape)
+    noisy_img = img + noise
+    noisy_img = np.clip(noisy_img, 0, 255).astype(np.uint8)
+    return noisy_img
+
+# Salt and pepper noise
+def salt_and_pepper_noise_adder(img, std_dev, mean = 0):
+    noise = np.random.saltan(mean, std_dev, img.shape)
+    noisy_img = img + noise
+    noisy_img = np.clip(noisy_img, 0, 255).astype(np.uint8)
+    return noisy_img
+
+cv.waitKey(0)
+cv.destroyAllWindows()
